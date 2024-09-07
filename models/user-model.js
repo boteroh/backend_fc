c21a5e53bb6295cbf96ba62809c8e133140d3288
Alejandro Botero <a.b.hincapie@gamail.com>
2024-09-06 22:37:47 -0500

    Create user model schema

--- models/user-model.js
+++ models/user-model.js
@@ -1,1 +1,39 @@
 import { model, Schema } from "mongoose";
+
+const userSchema = new Schema({
+    documentId: {
+        type: String,
+        unique: true,
+        required: true
+    },
+
+    names: {
+        type: String,
+        required: true
+    },
+
+    lastName: {
+        type: String,
+    },
+
+    area: {
+        type: String,
+        enum: [ 'Gerencia', 'Diseño', 'Desarrollo', 'Planeación', 'Compras', 'Comex' ],
+        required: true
+    },
+
+    role: {
+        type: String,
+        enum: [ 'Admin', 'Supervisor', 'Operational' ],
+        required: true
+    },
+
+    password: {
+        type: String,
+        minlength: 4,
+        maxlength: 10,
+        required: true
+    },
+});
+
+export default model( 'Users', userSchema, 'users' );
